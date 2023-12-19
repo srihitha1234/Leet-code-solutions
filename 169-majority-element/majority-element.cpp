8de03ad650d8a5bc19fe990cf802a85f75975a2b
srihitha Pulapa <124449649+srihitha1234@users.noreply.github.com>
2023-12-19 22:27:43 +0530

    Time: 19 ms (17.42%) | Memory: 20 MB (28.79%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -7,7 +7,6 @@ public:
             res[nums[i]]++;
         for(int i=0;i<n;i++)
         {
-            cout<<i<<" "<<res[nums[i]]<<endl;
             int maj=n/2;
             maj++;
             if(res[nums[i]]>=maj)
