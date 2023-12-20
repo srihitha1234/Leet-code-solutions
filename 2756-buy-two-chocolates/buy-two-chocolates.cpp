7ee5374a769b7e399ec711ceec0492ba037dec46
srihitha Pulapa <124449649+srihitha1234@users.noreply.github.com>
2023-12-20 10:42:25 +0530

    Time: 20 ms (13.52%) | Memory: 46.9 MB (58.30%) - LeetSync

--- 2756-buy-two-chocolates/buy-two-chocolates.cpp
+++ 2756-buy-two-chocolates/buy-two-chocolates.cpp
@@ -1,29 +1,33 @@
 class Solution {
 public:
-    int buyChoco(vector<int>& nums, int money) {
-        int min1,min2;
-        if(nums[0]>nums[1])
-        {
-            min1=nums[1];
-            min2=nums[0];
-        }
-        else
-        {
-            min1=nums[0];
-            min2=nums[1];
-        }
-        for(int i=2;i<nums.size();i++)
-        {
-            if(min1>nums[i])
-            {
-                min2=min1;
-                min1=nums[i];
+    int buyChoco(vector<int>& prices, int money) {
+        // Assume Minimum Cost to be Infinity
+        int minCost = INT_MAX;
+
+        // Number of Chocolates
+        int n = prices.size();
+
+        // Check Every Pair of Chocolates
+        for (int firstChoco = 0; firstChoco < n; firstChoco++) {
+            for (int secondChoco = firstChoco + 1; secondChoco < n; secondChoco++) {
+                // Sum of Prices of the Two Chocolates
+                int cost = prices[firstChoco] + prices[secondChoco];
+
+                // If the Sum of Prices is Less than the Minimum Cost
+                if (cost < minCost) {
+                    // Update the Minimum Cost
+                    minCost = cost;
+                }
             }
-            else if(min2>nums[i])
-                min2=nums[i];
         }
-        if(money-min1-min2<0)
+
+        // We can buy chocolates only if we have enough money
+        if (minCost <= money) {
+            // Return the Amount of Money Left
+            return money - minCost;
+        } else {
+            // We cannot buy chocolates. Return the initial amount of money
             return money;
-        return money-min1-min2;
+        }
     }
 };
