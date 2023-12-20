a7cae19baaddf80c72c324c536a1a7b78e3db4c6
srihitha Pulapa <124449649+srihitha1234@users.noreply.github.com>
2023-12-20 10:41:35 +0530

    Time: 14 ms (48.64%) | Memory: 47.1 MB (29.77%) - LeetSync

--- 2756-buy-two-chocolates/buy-two-chocolates.cpp
+++ 2756-buy-two-chocolates/buy-two-chocolates.cpp
@@ -1,14 +1,29 @@
-#include <vector>
-#include <algorithm>
-
 class Solution {
 public:
-    int buyChoco(std::vector<int>& nums, int money) {
-        if (nums.size() < 2) 
+    int buyChoco(vector<int>& nums, int money) {
+        int min1,min2;
+        if(nums[0]>nums[1])
+        {
+            min1=nums[1];
+            min2=nums[0];
+        }
+        else
+        {
+            min1=nums[0];
+            min2=nums[1];
+        }
+        for(int i=2;i<nums.size();i++)
+        {
+            if(min1>nums[i])
+            {
+                min2=min1;
+                min1=nums[i];
+            }
+            else if(min2>nums[i])
+                min2=nums[i];
+        }
+        if(money-min1-min2<0)
             return money;
-        sort(nums.begin(), nums.end());
-        if(money-nums[0]-nums[1]<0)
-            return money;
-        return money-nums[0]-nums[1];
+        return money-min1-min2;
     }
 };
