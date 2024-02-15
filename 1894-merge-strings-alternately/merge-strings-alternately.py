ff194c2f68bb36bb1803c7e317d9f35d28a03b7f
srihitha Pulapa <124449649+srihitha1234@users.noreply.github.com>
2024-02-15 13:00:35 +0530

    Time: 46 ms (11.77%) | Memory: 16.6 MB (57.91%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.py
+++ 1894-merge-strings-alternately/merge-strings-alternately.py
@@ -4,17 +4,11 @@ class Solution:
         i=0
         j=0
         for k in range(len(word1)+len(word2)):
-            if k%2==0 and i<len(word1):
+            if (k%2==0 and i<len(word1)) or (j>=len(word2)):
                 word=word+word1[i]
                 i=i+1
-            elif k%2!=0 and j<len(word2):
+            elif (k%2!=0 and j<len(word2)) or (i>=len(word1)):
                 word=word+word2[j]
                 j=j+1
-            elif i>=len(word1) :
-                word=word+word2[j]
-                j=j+1
-            else:
-                word=word+word1[i]
-                i=i+1
         return word
         
